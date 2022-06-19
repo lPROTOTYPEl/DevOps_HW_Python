4ef2e463fde9002b4315a368404760dedf3a8bd0
prototype <nikolaevjunior@gmail.com>
2022-06-19 18:59:20 +0300

    hw11

---
+++ hw11.py
@@ -0,0 +1,19 @@
+import string
+
+CHARS = string.ascii_lowercase
+
+def letters_range(start, stop, step=1):
+    start_i = CHARS.index(start)
+    stop_i = CHARS.index(stop)
+
+    return list(CHARS[start_i:stop_i:step])
+
+def main():
+    print(letters_range('b', 'w', 2))
+    print(letters_range('a', 'g'))
+    print(letters_range('g', 'p'))
+    print(letters_range('p', 'g', -2))
+    print(letters_range('a', 'a'))
+
+if __name__ == "__main__":
+    main()
