fa2f950716c74acaed8cfb3b2b17fc61b4795275
prototype <nikolaevjunior@gmail.com>
2022-06-19 18:57:15 +0300

    hw8

---
+++ hw8.py
@@ -0,0 +1,21 @@
+def text_handler():
+    string = input("-> ")
+
+    if string == "cancel":
+        print("Bye!")
+    else:
+        try:
+            if (x := int(string)) % 2 == 0:
+                print(x // 2)
+            else:
+                print(x * 3 + 1)
+        except ValueError:
+            print("Не удалось преобразовать введенный текст в число.")
+        finally:
+            return text_handler()
+
+def main():
+    text_handler()
+
+if __name__ == "__main__":
+    main()
