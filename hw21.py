71deb3336d6411557d71b60869a64d147a60638d
prototype <nikolaevjunior@gmail.com>
2022-07-10 13:54:36 +0300

    hw21

---
+++ hw21.py
@@ -0,0 +1,46 @@
+def merge(iter1, iter2):
+    try:
+        buf1 = next(iter1)
+    except StopIteration:
+        yield from iter2
+
+        return
+
+    try:
+        buf2 = next(iter2)
+    except StopIteration:
+        yield buf1
+        yield from iter1
+
+        return
+
+    while True:
+        if buf1 < buf2:
+            yield buf1
+
+            try:
+                buf1 = next(iter1)
+            except StopIteration:
+                yield buf2
+                yield from iter2
+
+                break
+
+        else:
+            yield buf2
+
+            try:
+                buf2 = next(iter2)
+            except StopIteration:
+                yield buf1
+                yield from iter1
+
+                break
+
+
+def main():
+    print(list(merge((x for x in range(1, 4)), (x for x in range(2, 5)))))
+
+
+if __name__ == '__main__':
+    main()
